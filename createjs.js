5ce10d0cb971fa0662fbc324bacf0c3ff56cc96c
kawamurashin <kawamura@mztm.jp>
2014-05-07 20:00:46 +0900

    first commit

---
+++ createjs.js
@@ -0,0 +1,22 @@
+(function (lib, img, cjs) {
+
+var p; // shortcut to reference prototypes
+
+// library properties:
+lib.properties = {
+	width: 465,
+	height: 465,
+	fps: 60,
+	color: "#FFFFFF",
+	manifest: []
+};
+
+// stage content:
+(lib.createjs = function() {
+	this.initialize();
+
+}).prototype = p = new cjs.Container();
+p.nominalBounds = null;
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{});
+var lib, images, createjs;
