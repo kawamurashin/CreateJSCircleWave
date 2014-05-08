6701f27d543d6d1165437b9342fd9efabf4440b7
kawamurashin <kawamura@mztm.jp>
2014-05-08 15:45:14 +0900

    とりあえず動く。もっとスマートなパッケージ化にしたい

--- CirclePoint.js
+++ CirclePoint.js
@@ -10,17 +10,52 @@
 var CirclePoint;
 CirclePoint = (function()
 {
-    var defaultX = 0;
-    var defaultY = 0;
+
+
+
     function CirclePoint()
     {
-        this.vw = 0;
-        this.localRotation = 0;
-
+        this.M = 1;
+        this.U = 0.01;
+        this.vx =0;
+        this.vy =0;
+        this.forceX = 0;
+        this.forceY = 0;
+        this.defaultX = 0;
+        this.defaultY = 0;
+        this._x =0;
+        this._y =0;
+    }
+    CirclePoint.prototype.onEnterFrame = function()
+    {
+        this.vx += (this.forceX) - this.U * this.vx;
+        this.vy += (this.forceY) - this.U * this.vy;
+        this._x += this.vx;
+        this._y += this.vy;
     }
     CirclePoint.prototype.hoge = function()
     {
         console.log("hoge");
+    }
+    CirclePoint.prototype.setPosition  = function(setX,setY)
+    {
+        this.defaultX =setX ;
+        this._x = this.defaultX;
+        this.defaultY = setY;
+        this._y  = this.defaultY;
+    }
+    CirclePoint.prototype.getPosition  = function()
+    {
+        return [this._x,this._y];
+    }
+    CirclePoint.prototype.getDefault  = function()
+    {
+        return [this.defaultX, this.defaultY];
+    }
+    CirclePoint.prototype.setForce  = function(fx ,fy )
+    {
+        this.forceX = fx;
+        this.forceY = fy;
     }
 
 
